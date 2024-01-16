f80cd5c9baf17bfff9db79b79812bcb2038a5390
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-16 22:14:23 +0100

    fix router

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
-
 import { useEffect } from 'react';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
-    Router.push('https://chat.openai.com/g/g-TXJFbP1Cg-nilsgpt');
+    router.push('https://chat.openai.com/g/g-TXJFbP1Cg-nilsgpt');
   }, []);
 
   return null;
