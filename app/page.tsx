a3a7c13b9ad44596139654753aff0c907585b75d
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-16 23:09:25 +0100

    add meta tags

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
     <Head>
       <title>Redirecting...</title>
       <meta property="og:title" content="Nils Westgardh's Personal Portfolio Website" />
-      <meta property="og:description" content="<My resume and portfolio is a ChatGPT bot.>" />
+      <meta property="og:description" content="<Nils Westgardh is an entrepeneur and advertising professional who's resume and portfolio is a ChatGPT bot.>" />
       <meta property="og:image" content="https://nilswestgardh.com/opengraph-image.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nilswestgardh.com/twitter-image.png" />
