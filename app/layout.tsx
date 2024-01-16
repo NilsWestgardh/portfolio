a9a718bfc6964b608f6485a5075900fdce2cbd54
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-01-16 23:29:02 +0100

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Nils Westgardh Personal Site',
-  description: 'My personal portfolio and resume is a ChatGPT bot: https://chat.openai.com/g/g-TXJFbP1Cg-nilsgpt'
-}
+  title: "Nils Westgardh's Personal Portfolio Website",
+  description: "Nils Westgardh is an entrepreneur and advertising professional whose resume and portfolio is a ChatGPT bot.",
+  openGraph: {
+    images: [
+      { url: "https://nilswestgardh.com/opengraph-image.png", alt: "Open Graph Image for Nils Westgardh's portfolio site." },
+      { url: "https://nilswestgardh.com/twitter-image.png", alt: "Twitter Image for Nils Westgardh's portfolio site." }
+    ],
+  }
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect } from 'react';
-import Head from 'next/head';
 
 export default function Page() {
   useEffect(() => {
@@ -11,13 +10,6 @@ export default function Page() {
   }, []);
 
   return (
-    <Head>
-      <title>Redirecting...</title>
-      <meta property="og:title" content="Nils Westgardh's Personal Portfolio Website" />
-      <meta property="og:description" content="<Nils Westgardh is an entrepeneur and advertising professional who's resume and portfolio is a ChatGPT bot.>" />
-      <meta property="og:image" content="https://nilswestgardh.com/opengraph-image.png" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nilswestgardh.com/twitter-image.png" />
-    </Head>
+    <title>Redirecting...</title>
   );
 }
